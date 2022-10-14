aca8d009ea56c668f81fdd4c0d7690b946be0366
izaan.jahangir <izaan.jahangir@aciano.net>
2022-10-14 12:18:47 +0500

    added get all items api

--- src/controllers/item/index.js
+++ src/controllers/item/index.js
@@ -1,5 +1,7 @@
 const { createItemValidation } = require("./validations");
 const Item = require("../../models/Item");
+const ItemType = require("../../models/itemType");
+const Cuisine = require("../../models/Cuisine");
 
 exports.createItem = async (req, res, next) => {
   try {
@@ -19,6 +21,31 @@ exports.createItem = async (req, res, next) => {
 
     res.json({
       data: { item },
+      message: "success",
+      success: true,
+    });
+  } catch (e) {
+    next({ message: e, status: e.status || 400 });
+  }
+};
+
+exports.getAllItems = async (req, res, next) => {
+  try {
+    const items = await Item.findAll({
+      include: [
+        {
+          model: Cuisine,
+          as: "cuisine",
+        },
+        {
+          model: ItemType,
+          as: "itemType",
+        },
+      ],
+    });
+
+    res.json({
+      data: { items },
       message: "success",
       success: true,
     });
--- src/routes/item.js
+++ src/routes/item.js
@@ -1,8 +1,9 @@
-const { createItem } = require("../controllers/item");
+const { createItem, getAllItems } = require("../controllers/item");
 const { isSuperAdmin } = require("../middlewares/authMiddlewares");
 
 const BASE_ROUTE = "/item";
 
 module.exports = (router) => {
   router.post(`${BASE_ROUTE}`, isSuperAdmin, createItem);
+  router.get(`${BASE_ROUTE}`, isSuperAdmin, getAllItems);
 };
