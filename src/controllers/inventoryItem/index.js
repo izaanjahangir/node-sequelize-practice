a23e5bc3bc78a10d34b0f68e8c00bbf4e5228a34
izaan.jahangir <izaan.jahangir@aciano.net>
2022-10-17 19:15:15 +0500

    added api to get all inventory items

--- src/controllers/inventoryItem/index.js
+++ src/controllers/inventoryItem/index.js
@@ -1,4 +1,5 @@
 const { createInventoryItemValidation } = require("./validations");
+const globalHelpers = require("../../utils/globalHelpers");
 const InventoryItem = require("../../models/InventoryItem");
 
 exports.createInventoryItem = async (req, res, next) => {
@@ -16,6 +17,36 @@ exports.createInventoryItem = async (req, res, next) => {
 
     res.json({
       data: { inventoryItem },
+      message: "success",
+      success: true,
+    });
+  } catch (e) {
+    next({ message: e, status: e.status || 400 });
+  }
+};
+
+exports.getInventoryItems = async (req, res, next) => {
+  try {
+    const page = req.query.page || 1;
+    const skipDoc = globalHelpers.calculateSkipDoc(page);
+    const limit = globalHelpers.getLimit(req.query.limit);
+
+    const totalItems = await InventoryItem.count();
+    const inventoryItems = await InventoryItem.findAll({
+      order: [["name", "asc"]],
+      offset: skipDoc,
+      limit: limit,
+    });
+
+    const totalPages = globalHelpers.calculateTotalPage(totalItems, limit);
+
+    res.json({
+      data: {
+        inventoryItems,
+        totalPages,
+        totalItems,
+        currentPage: Number(page),
+      },
       message: "success",
       success: true,
     });
--- src/routes/inventoryItem.js
+++ src/routes/inventoryItem.js
@@ -1,8 +1,9 @@
-const { createInventoryItem } = require("../controllers/inventoryItem");
+const { createInventoryItem, getInventoryItems } = require("../controllers/inventoryItem");
 const { isSuperAdmin } = require("../middlewares/authMiddlewares");
 
 const BASE_ROUTE = "/inventory-item";
 
 module.exports = (router) => {
   router.post(`${BASE_ROUTE}`, isSuperAdmin, createInventoryItem);
+  router.get(`${BASE_ROUTE}`, isSuperAdmin, getInventoryItems);
 };
