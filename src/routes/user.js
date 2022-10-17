8d5cbe670300c283dc063e61f2ff1807257a5c2d
izaan.jahangir <izaan.jahangir@aciano.net>
2022-10-17 23:59:44 +0500

    added docs for get all users

--- src/routes/user.js
+++ src/routes/user.js
@@ -30,6 +30,24 @@ module.exports = (router) => {
    * @apiError success contains "false"
    */
   router.post(`${BASE_ROUTE}`, isSuperAdmin, addUser);
+
+  /**
+   * @api {GET} /user Get all users
+   * @apiName Get all users
+   * @apiGroup User
+   *
+   * @apiHeader {String} Authorization token should be sent. In the following pattern Bearer {Token} replace by real token
+   *
+   * @apiHeaderExample {json} Header-Example:
+   *     {
+   *       "Authorization": "Bearer eyJhbGciOiJIUzI1N"
+   *     }
+   * @apiQuery {Number} page Page number
+   * @apiQuery {Number} [limit] Send 0 to fetch all records without pagination. Values other than 0 are ignored
+   *
+   * @apiError message contains the error message. will be an array if the error is more than one, for example validation failed
+   * @apiError success contains "false"
+   */
   router.get(`${BASE_ROUTE}`, isSuperAdmin, getAllUsers);
   router.post(`${BASE_ROUTE}/login`, loginUser);
   router.patch(`${BASE_ROUTE}/change-password`, isUser, changePassword);
