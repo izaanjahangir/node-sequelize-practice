4b85a8d3a80c1c241e2bb6d417668def927d7f08
izaan.jahangir <izaan.jahangir@aciano.net>
2022-10-18 20:13:12 +0500

    added api to get all kitchens

--- src/controllers/kitchen/index.js
+++ src/controllers/kitchen/index.js
@@ -1,5 +1,6 @@
 const { createKitchenValidation } = require("./validations");
 const Kitchen = require("../../models/Kitchen");
+const globalHelpers = require("../../utils/globalHelpers");
 
 exports.createKitchen = async (req, res, next) => {
   try {
@@ -16,6 +17,32 @@ exports.createKitchen = async (req, res, next) => {
 
     res.json({
       data: { kitchen },
+      message: "success",
+      success: true,
+    });
+  } catch (e) {
+    next({ message: e, status: e.status || 400 });
+  }
+};
+
+exports.getAllKitchens = async (req, res, next) => {
+  try {
+    const page = req.query.page || 1;
+    const skipDoc = globalHelpers.calculateSkipDoc(page);
+    const limit = globalHelpers.getLimit(req.query.limit);
+
+    const itemsResponse = await Kitchen.findAndCountAll({
+      order: [["kitchenNumber", "asc"]],
+      offset: skipDoc,
+      limit: limit,
+    });
+
+    const kitchens = itemsResponse.rows;
+    const totalItems = itemsResponse.count;
+    const totalPages = globalHelpers.calculateTotalPage(totalItems, limit);
+
+    res.json({
+      data: { kitchens, totalPages, totalItems, currentPage: Number(page) },
       message: "success",
       success: true,
     });
--- src/routes/kitchen.js
+++ src/routes/kitchen.js
@@ -1,4 +1,4 @@
-const { createKitchen } = require("../controllers/kitchen");
+const { createKitchen, getAllKitchens } = require("../controllers/kitchen");
 const { isSuperAdmin } = require("../middlewares/authMiddlewares");
 
 const BASE_ROUTE = "/kitchen";
@@ -23,4 +23,24 @@ module.exports = (router) => {
    * @apiError success contains "false"
    */
   router.post(`${BASE_ROUTE}`, isSuperAdmin, createKitchen);
+
+  /**
+   * @api {GET} /kitchen Get all kitchens
+   * @apiName Get all kitchens
+   * @apiGroup Kitchen
+   *
+   * @apiHeader {String} Authorization token should be sent. In the following pattern Bearer {Token} replace by real token
+   *
+   * @apiHeaderExample {json} Header-Example:
+   *     {
+   *       "Authorization": "Bearer eyJhbGciOiJIUzI1N"
+   *     }
+   *
+   * @apiQuery {Number} page Page number
+   * @apiQuery {Number} [limit] Send 0 to fetch all records without pagination. Values other than 0 are ignored
+   *
+   * @apiError message contains the error message. will be an array if the error is more than one, for example validation failed
+   * @apiError success contains "false"
+   */
+  router.get(`${BASE_ROUTE}`, isSuperAdmin, getAllKitchens);
 };
